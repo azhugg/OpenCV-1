f6a8917dc3bf773278d32b63bbed6e4404e53566
HyunJun Kim <me@hyunjun.org>
2015-05-02 03:27:14 +0900

    컬러 지원

--- MaxFilter.cpp
+++ MaxFilter.cpp
@@ -12,22 +12,27 @@
 using namespace std;
 using namespace cv;
 
+uchar intensity(Vec3b rgb) {
+    return (rgb[0] + rgb[1] + rgb[2]) / 3;
+}
+
 /**
  * 이미지에 Maximum Value Filter를 적용한다.
  * (인접한 픽셀 중 가장 greyscale값이 높은 픽셀값을 자신의 값으로 설정)
  *
  * @Mat& image - 변환될 이미지
  */
-void maxFilter(Mat& input, Mat& output) {
+ void maxFilter(Mat& input, Mat& output) {
     
     const int neighbors[8][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}};
     
-    output.create(input.rows, input.cols, CV_8UC1);
+    output.create(input.rows, input.cols, CV_8UC3);
     
     for (int y = 0; y < input.rows; y++) {
         for (int x = 0; x < input.cols; x++) {
             
             uchar maximum = 0;
+            Vec3b rgb;
             
             for (int i = 0; i < 8; i++) {
                 
@@ -37,12 +42,14 @@ void maxFilter(Mat& input, Mat& output) {
                 if(nx < 0 || ny < 0 || nx >= input.cols || ny >= input.rows)
                     continue;
                 
-                uchar greyscale = input.at<uchar>(ny, nx * input.channels());
+                uchar greyscale = intensity(input.at<Vec3b>(ny, nx));
                 
-                if(maximum < greyscale)
+                if(maximum < greyscale) {
                     maximum = greyscale;
+                    rgb = input.at<Vec3b>(ny, nx);
+                }
             }
-            output.at<uchar>(y, x) = maximum;
+            output.at<Vec3b>(y, x) = rgb;
         }
     }
 }
